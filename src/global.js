e118d6d96ac09555098d07bcd0e783bf8bc0d8c5
zyedibrahim <zyedibrahim77@gmail.com>
2023-03-16 17:32:31 +0530

    f

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
- export const API ="http://localhost:4000"
-//  export const API ="https://url-shortner-nodejs-3a1m.vercel.app"
+//  export const API ="http://localhost:4000" 
+ export const API ="https://url-shortner-nodejs-3a1m.vercel.app"
